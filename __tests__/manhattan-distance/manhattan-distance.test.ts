86c7a6fe2a0146a9aa49a1f5f74ae8817118977f
Antonio Leon <antonioleon@audiense.com>
2020-04-11 19:18:28 +0200

    Implement Manhattan distance kata

--- __tests__/manhattan-distance/manhattan-distance.test.ts
+++ __tests__/manhattan-distance/manhattan-distance.test.ts
@@ -1,5 +1,64 @@
-import { ManhattanDistance } from '../../src/manhattan-distance/manhattan-distance';
+import {
+  manhattanDistance,
+  Point,
+} from '../../src/manhattan-distance/manhattan-distance';
 
-describe('', () => {
-  it('', () => {});
+describe('ManhattanDistance', () => {
+  it('returns 0 if both points are in the same place', () => {
+    const VALUE = 20;
+    const pointA = new Point(VALUE, VALUE);
+    const pointB = new Point(VALUE, VALUE);
+
+    const distance = manhattanDistance(pointA, pointB);
+
+    expect(distance).toBe(0);
+  });
+
+  it('calculates the distance between two points in the same horizontal line', () => {
+    const X = 10;
+    const pointA = new Point(X, 5);
+    const pointB = new Point(X, 3);
+
+    const distance = manhattanDistance(pointA, pointB);
+
+    expect(distance).toBe(2);
+  });
+
+  it('calculates the distance between two points in the same vertical line', () => {
+    const Y = 30;
+    const pointA = new Point(5, Y);
+    const pointB = new Point(3, Y);
+
+    const distance = manhattanDistance(pointA, pointB);
+
+    expect(distance).toBe(2);
+  });
+
+  it('calculates the distance between two points that are in diagonal', () => {
+    const pointA = new Point(5, 4);
+    const pointB = new Point(3, 2);
+
+    const distance = manhattanDistance(pointA, pointB);
+
+    expect(distance).toBe(4);
+  });
+
+  it('calculates the distance when points are in different quadrants', () => {
+    const pointA = new Point(-5, 4);
+    const pointB = new Point(3, -2);
+
+    const distance = manhattanDistance(pointA, pointB);
+
+    expect(distance).toBe(14);
+  });
+
+  it('calculates the same distance independently of the points order', () => {
+    const pointA = new Point(5, 4);
+    const pointB = new Point(3, 2);
+
+    const directDistance = manhattanDistance(pointA, pointB);
+    const reverseDistance = manhattanDistance(pointB, pointA);
+
+    expect(directDistance).toBe(reverseDistance);
+  });
 });
--- src/manhattan-distance/manhattan-distance.ts
+++ src/manhattan-distance/manhattan-distance.ts
@@ -1,1 +1,20 @@
-export class ManhattanDistance {}
+export function manhattanDistance(pointA: Point, pointB: Point): number {
+  return pointA.distanceTo(pointB);
+}
+
+export class Point {
+  private readonly x: number;
+  private readonly y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  distanceTo(otherPoint: Point) {
+    const horizontalDistance = Math.abs(this.x - otherPoint.x);
+    const verticalDistance = Math.abs(this.y - otherPoint.y);
+
+    return horizontalDistance + verticalDistance;
+  }
+}
