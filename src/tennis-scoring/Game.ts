810ac7b5f350478089008af8c30b42fea70538dd
Antonio Leon <antonioleon@audiense.com>
2020-04-25 14:04:03 +0200

    Make players private

--- src/tennis-scoring/Game.ts
+++ src/tennis-scoring/Game.ts
@@ -4,8 +4,8 @@ import { Score } from '../../src/tennis-scoring/Score';
 export type PlayerNumber = 1 | 2;
 
 export class Game {
-  player1: Player;
-  player2: Player;
+  private player1: Player;
+  private player2: Player;
 
   constructor() {
     this.player1 = new Player();
