ebdf21818faa7af8f5605d09f56fd592b87a9083
Antonio Leon <antonioleon@audiense.com>
2020-05-01 22:31:37 +0200

    Refactor tests

--- __tests__/string-calculator/string-calculator.test.ts
+++ __tests__/string-calculator/string-calculator.test.ts
@@ -1,25 +1,20 @@
 import { StringCalculator } from '../../src/string-calculator/string-calculator';
 
 describe('String Calculator', () => {
+  const stringCalculator = new StringCalculator();
   it('returns 0 if does not receives values', () => {
-    const stringCalculator = new StringCalculator();
-
     const result = stringCalculator.add('');
 
     expect(result).toBe(0);
   });
 
   it('returns the same value if receives just one number', () => {
-    const stringCalculator = new StringCalculator();
-
     const result = stringCalculator.add('30');
 
     expect(result).toBe(30);
   });
 
   it('returns the sum of two values', () => {
-    const stringCalculator = new StringCalculator();
-
     const sum10plus10 = stringCalculator.add('10,10');
     const sum30plus10 = stringCalculator.add('10,30');
     const sum30plus30 = stringCalculator.add('30,30');
