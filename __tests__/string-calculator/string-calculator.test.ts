c74cffe9e6ae37cad049015a925c2eb56294b14f
Antonio Leon <antonioleon@audiense.com>
2020-05-01 22:11:01 +0200

    Implement case when does not receives numbers

--- __tests__/string-calculator/string-calculator.test.ts
+++ __tests__/string-calculator/string-calculator.test.ts
@@ -1,3 +1,11 @@
 import { StringCalculator } from '../../src/string-calculator/string-calculator';
 
-describe('String Calculator', () => {});
+describe('String Calculator', () => {
+  it('returns 0 if does not receives values', () => {
+    const stringCalculator = new StringCalculator();
+
+    const result = stringCalculator.add('');
+
+    expect(result).toBe(0);
+  });
+});
--- src/string-calculator/string-calculator.ts
+++ src/string-calculator/string-calculator.ts
@@ -1,1 +1,5 @@
-export class StringCalculator {}
+export class StringCalculator {
+  add(values: string): number {
+    return 0;
+  }
+}
