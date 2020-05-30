881e93b700a547123fe956ed53ad3888ea9656a0
Antonio Leon <antonioleon@audiense.com>
2020-05-30 21:45:49 +0200

    Refactor step

--- src/greeting/greeting.ts
+++ src/greeting/greeting.ts
@@ -13,26 +13,16 @@ export class Greeting {
     return `${this.HELLO}, ${curatedName}.`;
   }
 
-  private greetMultipleMixed(names: Array<string>): string {
-    const namesThatAreUpperCase = names.filter(this.isUpperCase);
-    const namesThatAreLowerCase = names.filter(name => !this.isUpperCase(name));
-
-    const greetForLowerCase = this.greetMultipleNormal(namesThatAreLowerCase);
-    const greetForUpperCase = 'AND ' + this.greetMultipleShouting(namesThatAreUpperCase);
-
-    return greetForLowerCase + ' ' + greetForUpperCase;
-  }
-
   private greetMultiple(names: Array<string>): string {
-    const hasUpperCaseNames = names.some(this.isUpperCase);
-    const hasLowerCaseNames = names.some(name => !this.isUpperCase(name));
+    const upperCaseNames = names.filter(this.isUpperCase);
+    const normalNames = names.filter(name => !this.isUpperCase(name));
 
-    if (hasLowerCaseNames && !hasUpperCaseNames) {
-      return this.greetMultipleNormal(names);
+    if (normalNames.length && !upperCaseNames.length) {
+      return this.greetMultipleNormal(normalNames);
     }
 
-    if (hasLowerCaseNames && hasUpperCaseNames) {
-      return this.greetMultipleMixed(names);
+    if(normalNames.length && upperCaseNames.length) {
+      return `${this.greetMultipleNormal(normalNames)} AND ${this.greetMultipleShouting(upperCaseNames)}`;
     }
   }
 
