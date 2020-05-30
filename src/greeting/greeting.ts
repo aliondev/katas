409d6fb3b7d1a7777d3a16c947adb14f0c7401a9
Antonio Leon <antonioleon@audiense.com>
2020-05-30 21:04:46 +0200

    Refactor

--- src/greeting/greeting.ts
+++ src/greeting/greeting.ts
@@ -1,7 +1,9 @@
 export class Greeting {
+  private readonly HELLO = 'Hello';
+
   private greetSingle(name: string): string {
     const curatedName = name === null ? 'my friend' : name;
-    const sentence = `Hello, ${curatedName}`;
+    const sentence = `${this.HELLO}, ${curatedName}`;
 
     if (curatedName.toUpperCase() === curatedName) {
       return `${sentence}!`.toUpperCase()
@@ -11,11 +13,16 @@ export class Greeting {
   }
 
   private greetMultiple(names: Array<string>): string {
+    const AND = 'and';
+
     if (names.length <= 2) {
-      return `Hello, ${names.join(' and ')}.`;
+      return `${this.HELLO}, ${names.join(` ${AND} `)}.`;
     }
 
-    return `Hello, ${names.slice(0, -1).join(', ')} and ${names[names.length -1]}.`;
+    const namesWithoutLast = names.slice(0, -1);
+    const lastOfNames = names[names.length - 1];
+
+    return `${this.HELLO}, ${namesWithoutLast.join(', ')} ${AND} ${lastOfNames}.`;
   }
 
   greet(target: string | Array<string>): string {
