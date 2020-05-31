c89c99e8bcb95d74330ce6e3d93e5f8560cb1529
Antonio Leon <antonioleon@audiense.com>
2020-05-31 18:53:40 +0200

    Remove redundant test checkings used during the developement of the implementation

--- __tests__/greeting/greeting.test.ts
+++ __tests__/greeting/greeting.test.ts
@@ -5,8 +5,6 @@ describe('Greeting kata', () => {
 
     it('has a method that perform a simple greeting with a name', () => {
       expect(greeting.greet('John')).toBe('Hello, John.');
-      expect(greeting.greet('Louis')).toBe('Hello, Louis.');
-      expect(greeting.greet('Thomas')).toBe('Hello, Thomas.');
     });
 
     it('handles null name greeting to a friend', () => {
@@ -15,32 +13,20 @@ describe('Greeting kata', () => {
 
     it('shouts to the person if the name has all its letters uppercase', () => {
       expect(greeting.greet('JOHN')).toBe('HELLO JOHN!');
-      expect(greeting.greet('LOUIS')).toBe('HELLO LOUIS!');
-      expect(greeting.greet('THOMAS')).toBe('HELLO THOMAS!');
     });
 
     it('handles two names', () => {
       expect(greeting.greet(['John', 'Louis'])).toBe('Hello, John and Louis.');
-      expect(greeting.greet(['Louis', 'John'])).toBe('Hello, Louis and John.');
     });
 
     it('handles multiple names', () => {
       expect(greeting.greet(['John', 'Louis', 'Tom']))
         .toBe('Hello, John, Louis and Tom.');
-
-      expect(greeting.greet(['Louis', 'Tom', 'John']))
-        .toBe('Hello, Louis, Tom and John.');
     });
 
     it('handles mix of shouted and normal names', () => {
-      expect(greeting.greet(['John', 'Louis', 'TOM']))
-        .toBe('Hello, John and Louis. AND HELLO TOM!');
-
       expect(greeting.greet(['John', 'LOUIS', 'Tom']))
         .toBe('Hello, John and Tom. AND HELLO LOUIS!');
-
-      expect(greeting.greet(['JOHN', 'LOUIS', 'Tom']))
-        .toBe('Hello, Tom. AND HELLO JOHN AND LOUIS!');
     });
 
     it('handles multiples shouted names', () => {
@@ -51,15 +37,9 @@ describe('Greeting kata', () => {
     it('splits names with commas in entries', () => {
       expect(greeting.greet(['John,Bob', 'Michael']))
         .toBe('Hello, John, Bob and Michael.');
-
-      expect(greeting.greet(['John', 'Bob, Michael']))
-        .toBe('Hello, John, Bob and Michael.');
     });
 
     it('allows intentional commas in names when scaped with double quotes', () => {
-      expect(greeting.greet(['John","Bob', 'Michael']))
-        .toBe('Hello, John,Bob and Michael.')
-
         expect(greeting.greet(['Thomas', 'George', 'John","Bob', 'Michael']))
         .toBe('Hello, Thomas, George, John,Bob and Michael.')
     });
